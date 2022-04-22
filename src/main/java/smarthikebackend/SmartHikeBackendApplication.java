fbc2b1008fc1d2ee9b0af47ce2a349ac31ed29ae
actual_youknowho <asrarzaman.m@gmail.com>
2022-04-22 16:21:52 -0700

    spring-boot-refactor

---
+++ src/main/java/smarthikebackend/SmartHikeBackendApplication.java
@@ -0,0 +1,13 @@
+package smarthikebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHikeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartHikeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/smarthikebackend/SmartHikeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package smarthikebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHikeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
